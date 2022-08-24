fff7cab854e0acfe5ea9008c6fb31343345223fb
MicaellyUchoa <micaelly.santos@picpay.com>
2022-08-23 22:40:59 -0300

    feat: created section for informations of teacher

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,7 +1,51 @@
+import { DiscordLogo } from "phosphor-react";
+
 export function Video() {
   return (
-    <div className="sm:w-full md:w-full bg-slate-500 flex justify-center items-center">
-      <p className="text-4xl">Video</p>
+    <div className=" flex-1">
+      <section className="bg-black flex justify-center">
+        <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video"></div>
+      </section>
+      <section className="p-8 max-w-[1100px] mx-auto">
+        <div className="flex items-start gap-16">
+          <div className="flex-1">
+            <h1 className="text-2xl font-bold">Aula 01 - Criando o projeto e realizando o setup inicial</h1>
+            <p className="mt-4 text-gray-200 leading-relaxed">
+              Nessa aula vamos dar início ao projeto criando a estrutura base da aplicação utilizando ReactJS, Vite e
+              TailwindCSS. Vamos também realizar o setup do nosso projeto no GraphCMS criando as entidades da aplicação
+              e integrando a API GraphQL gerada pela plataforma no nosso front-end utilizando Apollo Client.
+            </p>
+
+            <div className="flex items-center gap-4 mt-6">
+              <img
+                className="h-16 w-16 rounded-full border-2 border-blue-500"
+                src="https://avatars.githubusercontent.com/u/37215778?v=4"
+                alt="Micaelly Santos"
+              />
+              <div className="leading-relaxed">
+                <strong className="font-bold text-2xl block">Micaelly Santos</strong>
+                <span className="text-gray-200 text-sm block">Dev Frontend</span>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col gap-4">
+            <a
+              href="#"
+              className="p-4 text-sm bg-green-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-green-700 transition-colors"
+            >
+              <DiscordLogo size={24} />
+              Comunidade do Discord
+            </a>
+            <a
+              href="#"
+              className="p-4 text-sm border border-blue-500 text-blue-500  flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-blue-500 hover:text-gray-900 transition-colors"
+            >
+              <DiscordLogo size={24} />
+              Acesse o desafio
+            </a>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
