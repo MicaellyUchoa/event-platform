b67ef453ce3d3de3a992e5ed79b82047e06c792d
MicaellyUchoa <micaelly.santos@picpay.com>
2022-08-25 00:15:45 -0300

    feat: add hover on lesson

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -8,9 +8,9 @@ export function Lesson(props: ILesson) {
   const availableDateFormatted = format(props.availableAt, "EEEE ' • 'd  ' de 'MMM' • 'k'h'mm", { locale: prBR });
 
   return (
-    <a href="#">
+    <a href="#" className="group">
       <span className="text-gray-300">{availableDateFormatted}</span>
-      <div className="rounded border border-gray-500 p-4 mt-2">
+      <div className="rounded border border-gray-500 group-hover:border-green-500 p-4 mt-2">
         <header className="flex items-center justify-between">
           {isLessonAvailable ? (
             <span className="text-sm text-blue-500 font-medium flex items-center gap-2">
