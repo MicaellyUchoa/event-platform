009e1ed7e00d460c9a85571dfa46a1494d9e04a5
MicaellyUchoa <micaellyit@hotmail.com>
2022-09-02 16:13:23 -0300

    feat: create environments

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: "https://api-sa-east-1.hygraph.com/v2/cl72i83u42t3m01uh6joo47ze/master",
+  uri: import.meta.env.VITE_API_URL,
+  headers: {
+    Authorization: `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`,
+  },
   cache: new InMemoryCache(),
 });
